1f9917588c70af7def7f83861c226a7505d89443
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-05 09:48:35 +0430

    done

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,8 @@
 let year = 2023;
 let dateOfBirth = 2001;
 
-let age = "I am Esmtullah a " + (year - dateOfBirth) + " years old!";
+let age1 = "I am Esmtullah a " + (year - dateOfBirth) + " years old!";
+console.log(age1);
+
+let age = `I am Esmatullah a ${year - dateOfBirth} years old`;
 console.log(age);
