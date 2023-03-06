e9163259986ac09459d8dfdf1cf10625304b37ea
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-06 13:56:22 +0430

    Add react

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,29 @@
-let year = 2023;
-let dateOfBirth = 2001;
+// let year = 2023;
+// let dateOfBirth = 2001;
 
-let age1 = "I am Esmtullah a " + (year - dateOfBirth) + " years old!";
-console.log(age1);
+// let age1 = "I am Esmtullah a " + (year - dateOfBirth) + " years old!";
+// console.log(age1);
 
-let age = `I am Esmatullah a ${year - dateOfBirth} years old`;
-console.log(age);
+// let age = `I am Esmatullah a ${year - dateOfBirth} years old`;
+// console.log(age);
+
+
+// Array Operation or Methods
+
+const dudes = ['Esmatullah', 'Shafiqa', 'Haida'];
+console.log(dudes);
+let firstWay = [];
+
+const ages = new Array(2000, 2001, 20004);
+console.log(ages);
+
+let numbers  = new Array();
+numbers[0] = 1000;
+console.log(numbers);
+
+firstWay[0] = 5000;
+console.log(firstWay);
+
+// Array based indexes Esmatullah, Shafiqa,
+console.log(dudes)
+
