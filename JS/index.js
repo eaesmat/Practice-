8f285548c0c63f52a0e260911b50039ddc6c0be6
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-07 09:31:18 +0430

    Work how to use Array methods

--- JS/index.js
+++ JS/index.js
@@ -10,20 +10,46 @@
 
 // Array Operation or Methods
 
-const dudes = ['Esmatullah', 'Shafiqa', 'Haida'];
-console.log(dudes);
-let firstWay = [];
+// const dudes = ['Esmatullah', 'Shafiqa', 'Haida'];
+// console.log(dudes);
+// let firstWay = [];
 
-const ages = new Array(2000, 2001, 20004);
-console.log(ages);
+// const ages = new Array(2000, 2001, 20004);
+// console.log(ages);
 
-let numbers  = new Array();
-numbers[0] = 1000;
-console.log(numbers);
+// let numbers  = new Array();
+// numbers[0] = 1000;
+// console.log(numbers);
 
-firstWay[0] = 5000;
-console.log(firstWay);
+// firstWay[0] = 5000;
+// console.log(firstWay);
 
-// Array based indexes Esmatullah, Shafiqa,
-console.log(dudes)
+// // Array based indexes Esmatullah, Shafiqa,
+// console.log(dudes)
 
+const friends = ['Esmatullah', 'Shafiqa', 'Hadia'];
+// console.log(friends);
+
+//We talk about some important Array methods
+// 1. the push() method that is used to add and array element at the end
+
+// friends.push('Marwa');
+// const ret = friends.unshift('Marwa');
+// const ret = friends.shift();
+const ret = friends.includes('Marwa');
+console.log(friends, ret);
+
+const numbers = [12, 23, 50, 90, 100, 999, 1000, 987, 682];
+const odd = [];
+const even = new Array();
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 === 0) {
+        even.push(numbers[i]);
+    }
+    else {
+        odd.push(numbers[i]);
+    }
+}
+
+console.log(even, odd);
