1150c66f543d5b98e2143c77cf60d4d80005e5fb
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-11 10:42:38 +0430

    check range of the score

--- starter/script.js
+++ starter/script.js
@@ -6,39 +6,49 @@ const randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 // This is a method called on click of check button
-document.querySelector('.check').addEventListener('click', function(){
+document.querySelector('.check').addEventListener('click', function () {
+  // Store the guess number form the user input
+  const guessInput = document.querySelector('.guess').value;
 
-    // Store the guess number form the user input
-    const guessInput = document.querySelector('.guess').value;
+  document.querySelector('.number').textContent = randomNumber;
+
+  // Check if the number is not empty
+  if (!guessInput) {
+    document.querySelector('.message').textContent = 'Please enter number!';
+  }
+
+  // check if the guess and random numbers are matched
+  else if (guessInput == randomNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  }
+
+  // check if the number is higher
+  else if (guessInput > randomNumber) {
+    document.querySelector('.message').textContent = 'Very High!';
 
 
-    document.querySelector('.number').textContent = randomNumber;
-
-    // Check if the number is not empty
-    if(!guessInput)
-    {
-        document.querySelector('.message').textContent = "Please enter number!";
+    // check if the numnber is lower than 1
+    if (score > 0) {
+      score--;
+      document.querySelector('.score').textContent = score;
     }
-
-    // check if the guess and random numbers are matched
-    else if(guessInput == randomNumber)
-    {
-        document.querySelector('.message').textContent = "Correct number!";
+    else {
+        document.querySelector('.message').textContent = 'Lost the game';
     }
+  }
 
-    // check if the number is higher
-    else if(guessInput > randomNumber)
-    {
-        document.querySelector('.message').textContent = "Very High!";
-        score --;
+  // check if the number lower
+  else if (guessInput < randomNumber) {
+    document.querySelector('.message').textContent = 'Very Low!';
+
+
+    // check if the numnber is lower than 1
+    if (score > 0) {
+        score--;
         document.querySelector('.score').textContent = score;
-    }
-
-    // check if the number lower
-    else if(guessInput < randomNumber)
-    {
-        document.querySelector('.message').textContent = "Very Low!";
-        score --;
-        document.querySelector('.score').textContent = score;
-    }
+      }
+      else {
+          document.querySelector('.message').textContent = 'Lost the game';
+      }
+  }
 });
