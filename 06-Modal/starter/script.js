d9765c405d6bfb8ba8b3faa20ff039b87df44876
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-12 10:15:03 +0430

    Select are doms

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+// Select all requred DOMs
+const modalShowBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for(let modalItem = 0; modalItem < modalShowBtns.length; modalItem++) {
+    console.log(modalShowBtns[modalItem].textContent);
+}
+
