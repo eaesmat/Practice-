45517c1b0d4fe83660067cb9f53f266ada9cd0d0
eaesmat <ea.ahmadzai2020@gmail.com>
2023-03-27 11:31:44 +0430

    select the elements

--- starter Pig Game/script.js
+++ starter Pig Game/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Select the Elements
+const scoreZeroElement = document.querySelector('#score--0');
+const ScoreOneElement = document.querySelector('#score--1');
+// const ScoreOneElement = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+// Reset values
+scoreZeroElement.textContent = 0;
+ScoreOneElement.textContent = 0;
+// dice.style.display = 'none';
+dice.classList.add('hidden');
+
